026ce4179d2b6179ca849fbe755c2792a52329cd
liyuehui001 <1494230056@qq.com>
2017-06-16 11:27:18 +0800

    push

--- api_documents/submit_test.md
+++ api_documents/submit_test.md
@@ -1,8 +1,93 @@
 #测试
 
+##
+
+###1. <a name="getApi">获取api的所有信息</a>
 
 	get /apiManagerEndCode/src/submit_test.php
-request:
+	request:
+	{
+		api_id:(api的id)
+	}
+
+	response:
+	{
+		requests:
+			[
+				{
+					id:
+					key:
+					desc:
+					type:
+					rank:
+					parent:
+					api_id:
+					required:
+				},
+				{
+					id:
+					key:
+					desc:
+					type:
+					rank:
+					parent:
+					api_id:
+					required:
+				}
+			],
+		request_heads:[
+				{
+					id:
+					head:
+					api_id:
+					name:
+				},
+				{
+					id:
+					head:
+					api_id:
+					name:
+				},
+			],
+
+		responses:[
+				{
+					id:
+					key:
+					desc:
+					type:
+					rank:
+					parent:
+					api_id:
+					required:
+				},
+				{
+					id:
+					key:
+					desc:
+					type:
+					rank:
+					parent:
+					api_id:
+					required:
+				}
+			]
+		
+	}
+
+
+2. <a name='test'>测试开始</a>
+
+
+	post /apiManagerEndCode/src/submit_test.php
+	request:
+	{
+		api_id:
+		根据api_info 的参数来设置
+	}
+
+	response:
 {
-	
+	result:(0|1)
+	msg:错误信息
 }
---
+++ src/submit_test.php
@@ -0,0 +1,94 @@
+<?php
+include $_SERVER['DOCUMENT_ROOT'].'/apiManagerEndCode/src/MySqlPDO.class.php';
+docs();
+
+function docs(){
+	session_start();
+	include $_SERVER['DOCUMENT_ROOT'].'/apiManagerEndCode/src/request.php';
+	switch ($request) {
+		case 'GET':
+			doget($data);
+			break;
+		
+		default:
+			# code...
+			break;
+	}
+
+}
+
+
+function doget($data){
+	if ( isset($data['api_id']) && !empty($data['api_id']) ) {
+		$api_id = $data['api_id'];
+		$mysqlpdo = new MySqlPDO();
+		//获取requests
+		$select_request = "select * from `api_info` where `api_id` = ?";
+		$myarray = array( $api_id );
+		$mysqlpdo->prepare($select_request);
+		if ($mysqlpdo->executeArr($myarray)) {
+			$result['requests'] = array();
+			while($rs = $mysqlpdo->fetch()){
+				$temp = array();
+				foreach ($rs as $key => $value) {
+					$temp[$key]=$value;
+				}
+				$result['requests'][]=$temp;
+			}
+		}else{
+			$result['result']=0;
+			$result['msg']="获取请求错误";
+			echo json_encode($result);
+			return ;
+		}
+
+		$select_reqeust_heads = "select * from `request_head` where `api_id`=?";
+		$mysqlpdo->prepare($select_reqeust_heads);
+		if ($mysqlpdo->executeArr($myarray)) {
+			$result['request_heads'] = array();
+			while($rs = $mysqlpdo->fetch()){
+				$temp = array();
+				foreach ($rs as $key => $value) {
+					$temp[$key]=$value;
+				}
+				$result['request_heads'][]=$temp;
+			}
+		}else{
+			$result['result']=0;
+			$result['msg']="获取请求头错误";
+			echo json_encode($result);
+			return ;
+		}
+
+
+		$select_responses = "select * from `response_api` where `api_id`=?";
+		$mysqlpdo->prepare($select_responses);
+		if ($mysqlpdo->executeArr($myarray)) {
+			$result['responses'] = array();
+			while($rs = $mysqlpdo->fetch()){
+				$temp = array();
+				foreach ($rs as $key => $value) {
+					$temp[$key]=$value;
+				}
+				$result['responses'][]=$temp;
+			}
+		}else{
+			$result['result']=0;
+			$result['msg']="获取相应参数错误";
+			echo json_encode($result);
+			return ;
+		}
+
+
+
+
+		$result['result']=1;
+		echo json_encode($result);
+
+
+
+	}
+}
+
+
+?>
